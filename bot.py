05f723f3904c48e46988cdc90f15676db75cedf4
fabricehope1 <turikumwenayofabrice1@gmail.com>
2026-04-20 17:05:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import os
 import time
 import threading
 from threading import Thread
-from session import run_session
 from session_manager import session_manager
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
